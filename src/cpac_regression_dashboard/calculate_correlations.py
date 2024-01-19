71bb23bbbcb550a8effbd1135d395e56fafc1e63
Jon Clucas <jon.clucas@childmind.org>
2024-01-19 18:34:19 -0500

    :package: Make `cpac_regsuite_correlate` a CLI script

--- src/cpac_regression_dashboard/calculate_correlations.py
+++ src/cpac_regression_dashboard/calculate_correlations.py
@@ -4,8 +4,15 @@ from cpac_correlations import cpac_correlations
 
 from .utils.html_script import body
 
-if __name__ == "__main__":
+
+def main() -> None:  # noqa: D103
     all_keys, data_source, branch = cpac_correlations()
     html_body = body(all_keys, data_source)
     with open(f"{data_source}_{branch}.json", "w", encoding="utf-8") as file:
         file.write(html_body)
+
+
+main.__doc__ = __doc__
+
+if __name__ == "__main__":
+    main()
